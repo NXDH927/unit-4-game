4b6d96013f17445f377a6b0a791144ab67114a8c
Niles Dyer-Hunter <NXDH927@outlook.com>
2019-06-03 10:39:29 -0600

    HomeworkUpdate

---
+++ javascript/script.js
@@ -0,0 +1,95 @@
+
+ var wins=0
+ var losses=0
+ var score=0;
+ var goal=0;
+
+$(document).ready(function(){
+    
+
+    var crystalRedValue=0;
+
+
+    function goalFunction(){
+
+        var goalUnrounded = Math.random() * (120 - 19) + 19;
+        goal = Math.floor(goalUnrounded);
+        $("#goal").text(goal);
+        console.log(goal);
+
+    }
+
+    function crystalRandomFunction(){
+
+        crystalRedValue = Math.floor(Math.random() * (12 - 1) + 1);
+        $("#R").data("value", crystalRedValue);
+
+        var crystalBlueValue = Math.floor(Math.random() * (12 - 1) + 1);
+        $("#B").data("value", crystalBlueValue);
+
+        var crystalGreenValue = Math.floor(Math.random() * (12 - 1) + 1);
+        $("#G").data("value", crystalGreenValue);
+
+        var crystalYellowValue = Math.floor(Math.random() * (12 - 1) + 1);
+        $("#Y").data("value", crystalYellowValue);
+
+    }
+
+    $("#R").click(function() {
+        var value = $(this).data("value");
+        score = score + value;
+        $("#score").text(score);
+    });
+
+    $("#B").click(function() {
+        var value = $(this).data("value");
+        score = score + value;
+        $("#score").text(score);
+    });
+
+    $("#G").click(function() {
+        var value = $(this).data("value");
+        score = score + value;
+        $("#score").text(score);
+    });
+
+    $("#Y").click(function() {
+        var value = $(this).data("value");
+        score = score + value;
+        $("#score").text(score);
+    });
+
+    $(".Crystal").click(function(){
+
+        if(score === goal){
+
+            wins++;
+            $("#wins").text(wins);
+            console.log(wins);
+            goal=0;
+            score=0;
+            goalFunction();
+            crystalRandomFunction();
+
+
+
+        }
+        
+        if(score >= goal){
+
+            losses++;
+            $("#losses").text(losses);
+            console.log(losses);
+            goal=0;
+            score=0;
+            goalFunction();
+            crystalRandomFunction();
+        }
+
+
+    });
+
+    goalFunction();
+    crystalRandomFunction();
+  });
+
